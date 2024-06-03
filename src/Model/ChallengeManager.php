089e351ef2c0f3b464b82215eceb323c7448d442
Cyril <cyril.leconte07@gmail.com>
2024-06-03 14:04:15 +0200

    updated file changes

--- src/Model/ChallengeManager.php
+++ src/Model/ChallengeManager.php
@@ -8,8 +8,6 @@ class ChallengeManager extends AbstractManager
 {
     public const TABLE = 'challenge';
 
-    public array $assets;
-
     public function __construct()
     {
         parent::__construct();
