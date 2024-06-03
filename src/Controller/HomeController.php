af4d1ee1c1e3adb793c22676ccbe6b67eb0d85c2
Cyril <cyril.leconte07@gmail.com>
2024-06-03 21:40:22 +0200

    removed unused room pics

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,8 +8,8 @@ class HomeController extends AbstractController
      * Display home page
      */
 
-     public function index(): string
-     {
-         return $this->twig->render('/Home/index.html.twig');
-     }
-} 
+    public function index(): string
+    {
+        return $this->twig->render('/Home/index.html.twig');
+    }
+}
