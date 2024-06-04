1ef72e0d9d6245d9673a2d804e300297ebcea214
AnnaGosme <anna.gosme@gmail.com>
2024-06-05 00:16:57 +0200

    add save datetime

--- src/Model/SaveManager.php
+++ src/Model/SaveManager.php
@@ -13,11 +13,14 @@ class SaveManager extends AbstractManager
      */
     public function saveProgress(int $userId, int $challengeId): void
     {
+        $datetime = date('Y-m-d H:i:s');
+
         $statement = $this->pdo->prepare("INSERT INTO  " . static::TABLE . " 
-        (user_id, challenge_id) 
-        VALUES (:user_id, :challenge_id)");
+        (user_id, challenge_id, created_at) 
+        VALUES (:user_id, :challenge_id, :date_time)");
         $statement->bindValue(':user_id', $userId, \PDO::PARAM_INT);
         $statement->bindValue(':challenge_id', $challengeId, \PDO::PARAM_INT);
+        $statement->bindValue(':date_time', $datetime, \PDO::PARAM_STR);
         $statement->execute();
     }
 }
