dffbb4c520e04c75a07c493a3c97a3833fdc6470
AnnaGosme <anna.gosme@gmail.com>
2024-06-05 00:36:02 +0200

    correct following PR comments

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -3,25 +3,17 @@
 namespace App\Controller;
 
 use App\Model\RoomManager;
+use App\Model\ChallengeManager;
 
 class RoomController extends AbstractController
 {
-    public function getChallenge($challengeId): array
-    {
-        $roomManager = new RoomManager();
-        $challenge = $roomManager->getChallenge($challengeId);
-        return $challenge;
-    }
-
     public function show($roomId, $challengeId): string
     {
-        // $roomId = (int) $roomId;
-        // $challengeId = (int) $challengeId;
-
         $roomManager = new RoomManager();
         $room = $roomManager->selectOneById($roomId);
 
-        $challenge = $this->getChallenge($challengeId);
+        $challengeManager = new ChallengeManager();
+        $challenge = $challengeManager->selectOneById($challengeId);
 
         return $this->twig->render('Rooms/room.html.twig', [
             'room' => $room,
--- src/Model/RoomManager.php
+++ src/Model/RoomManager.php
@@ -5,23 +5,4 @@ namespace App\Model;
 class RoomManager extends AbstractManager
 {
     public const TABLE = 'room';
-
-    public function selectNextRoom(int $id): array|false
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("SELECT next FROM " . static::TABLE . " WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-
-        return $statement->fetch();
-    }
-
-    public function getChallenge(int $challengeId)
-    {
-        $statement = $this->pdo->prepare("SELECT * FROM challenge WHERE id=:id");
-        $statement->bindValue('id', $challengeId, \PDO::PARAM_INT);
-        $statement->execute();
-
-        return $statement->fetch();
-    }
 }
