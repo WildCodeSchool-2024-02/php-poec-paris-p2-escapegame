9edf850587cb566692ae45f020678388efe0c6f0
AnnaGosme <anna.gosme@gmail.com>
2024-06-04 02:11:57 +0200

    add assets for puzzles id6 id9, correct game logic, correct answer key in database, add side boxes for puzzle rearrange

--- src/Controller/ChallengeController.php
+++ src/Controller/ChallengeController.php
@@ -8,6 +8,25 @@ use RangeException;
 
 class ChallengeController extends AbstractController
 {
+    public $images = [
+        '1.png',
+        '2.png',
+        '3.png',
+        '4.png',
+        '5.png',
+        '6.png',
+        '7.png',
+        '8.png',
+        '9.png'
+    ];
+
+    public function shuffleImages()
+    {
+        $shuffledImages = $this->images;
+        shuffle($shuffledImages);
+        return $shuffledImages;
+    }
+
     public array $data;
     /**
      * Show informations for a specific challenge
@@ -15,11 +34,14 @@ class ChallengeController extends AbstractController
 
     public function show(int $id): string
     {
+
         $challengeManager = new ChallengeManager();
         $challenge = $challengeManager->selectOneById($id);
 
+
         return $this->twig->render('Challenges/' . $challenge['type'] . '.html.twig', [
             'challenge' => $challenge,
+            'images' => $this->shuffleImages(),
         ]);
     }
 
@@ -30,6 +52,9 @@ class ChallengeController extends AbstractController
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $jsonString = $_POST['userSolution'];
             $userSolution = json_decode($jsonString, true);
+            $userSolution = array_map(function ($filename) {
+                return preg_replace('/\.png$/', '', $filename);
+            }, $userSolution);
             $userSolution = array_map('trim', array_map('htmlentities', $userSolution));
             $userSolutionString = implode('', $userSolution);
 
