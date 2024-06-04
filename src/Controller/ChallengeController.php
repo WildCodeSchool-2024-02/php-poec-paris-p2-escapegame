01c00858af293dd837dad2d04abdb5c4ed350af3
AnnaGosme <anna.gosme@gmail.com>
2024-06-04 17:45:21 +0200

    correct following review

--- src/Controller/ChallengeController.php
+++ src/Controller/ChallengeController.php
@@ -8,40 +8,17 @@ use RangeException;
 
 class ChallengeController extends AbstractController
 {
-    public $images = [
-        '1.png',
-        '2.png',
-        '3.png',
-        '4.png',
-        '5.png',
-        '6.png',
-        '7.png',
-        '8.png',
-        '9.png'
-    ];
-
-    public function shuffleImages()
-    {
-        $shuffledImages = $this->images;
-        shuffle($shuffledImages);
-        return $shuffledImages;
-    }
-
-    public array $data;
     /**
      * Show informations for a specific challenge
      */
 
-    public function show(int $id): string
+    public function get(int $id): string
     {
-
         $challengeManager = new ChallengeManager();
         $challenge = $challengeManager->selectOneById($id);
 
-
         return $this->twig->render('Challenges/' . $challenge['type'] . '.html.twig', [
             'challenge' => $challenge,
-            'images' => $this->shuffleImages(),
         ]);
     }
 
@@ -50,25 +27,19 @@ class ChallengeController extends AbstractController
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $jsonString = $_POST['userSolution'];
             $userSolution = json_decode($jsonString, true);
-            $userSolution = array_map(function ($filename) {
-                return preg_replace('/\.png$/', '', $filename);
+            $userSolution = array_map(function ($userSolution) {
+                return preg_replace('/\.png$/', '', $userSolution);
             }, $userSolution);
             $userSolution = array_map('trim', array_map('htmlentities', $userSolution));
             $userSolutionString = implode('', $userSolution);
-
             $challengeManager = new ChallengeManager();
-            $correctAnswer = $challengeManager->selectChallengeAnswer($id);
             $challenge = $challengeManager->selectOneById($id);
 
-            if (strlen($userSolutionString) === strlen($correctAnswer['answer'])) {
-                $isCorrect = $userSolutionString === $correctAnswer['answer'];
-                return $this->twig->render('Challenges/validate.html.twig', [
-                    'isCorrect' => $isCorrect,
-                    'challenge' => $challenge
-                ]);
-            } else {
-                throw new Exception('Too few arguments');
-            }
+            $isCorrect = $userSolutionString === $challenge['answer'];
+            return $this->twig->render('Challenges/validate.html.twig', [
+                'isCorrect' => $isCorrect,
+                'challenge' => $challenge
+            ]);
         }
     }
 }
--- src/Model/ChallengeManager.php
+++ src/Model/ChallengeManager.php
@@ -8,31 +8,8 @@ class ChallengeManager extends AbstractManager
 {
     public const TABLE = 'challenge';
 
-    public array $assets;
     public function __construct()
     {
         parent::__construct();
-    }
-
-    /**
-     * Get one row from database by ID.
-     */
-    public function selectType(int $id): array|false
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("SELECT type FROM " . static::TABLE . " WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-
-        return $statement->fetch();
-    }
-
-    public function selectAnswer(int $id): array|false
-    {
-        $statement = $this->pdo->prepare("SELECT answer FROM " . static::TABLE . " WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-
-        return $statement->fetch();
     }
 }
