4d36493c2aa51e1699daa8434d1e62cc9fec17ce
AnnaGosme <anna.gosme@gmail.com>
2024-06-05 11:36:52 +0200

    refactor

--- src/Controller/ChallengeController.php
+++ src/Controller/ChallengeController.php
@@ -8,12 +8,11 @@ use RangeException;
 
 class ChallengeController extends AbstractController
 {
-    public $userSolutionString = "";
     /**
      * Show informations for a specific challenge
      */
 
-    public function show(int $id): string
+    public function get(int $id): string
     {
         $challengeManager = new ChallengeManager();
         $challenge = $challengeManager->selectOneById($id);
@@ -23,32 +22,14 @@ class ChallengeController extends AbstractController
         ]);
     }
 
-    public function prepareUserSolution($userSolution)
-    {
-        $jsonString = $userSolution;
-        $userSolution = json_decode($jsonString, true);
-        $userSolution = array_map(function ($userSolution) {
-            return preg_replace('/\.png$/', '', $userSolution);
-        }, $userSolution);
-        $userSolution = array_map('trim', array_map('htmlentities', $userSolution));
-        $userSolutionString = implode('', $userSolution);
-        return $userSolutionString;
-    }
-
     public function validate(int $id)
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (isset($_POST['userPuzzleSolution'])) {
-                $this->userSolutionString = $this->prepareUserSolution($_POST['userPuzzleSolution']);
-            }
-            // add other challenge submissions
-            $challengeManager = new ChallengeManager();
-            $challenge = $challengeManager->selectOneById($id);
-            $isCorrect = $this->userSolutionString === $challenge['answer'];
-            return $this->twig->render('Challenges/validate.html.twig', [
-                'isCorrect' => $isCorrect,
-                'challenge' => $challenge
-            ]);
-        }
+        $challengeManager = new ChallengeManager();
+        $challenge = $challengeManager->selectOneById($id);
+
+        return $this->twig->render('Challenges/validate.html.twig', [
+            'isCorrect' => $_POST['user_answer'] === $challenge['answer'],
+            'challenge' => $challenge
+        ]);
     }
 }
