25aa6ea61d2560ecf3451c51f834f495a6c0c332
Cyril <cyril.leconte07@gmail.com>
2024-06-07 00:32:09 +0200

    fixed issue, create result page and fixed bug link to redirect

--- src/Controller/ChallengeController.php
+++ src/Controller/ChallengeController.php
@@ -2,32 +2,49 @@
 
 namespace App\Controller;
 
-use Exception;
 use App\Model\ChallengeManager;
-use RangeException;
+use App\Model\SaveManager;
 
 class ChallengeController extends AbstractController
 {
+    private ChallengeManager $challengeManager;
+    // private SaveManager $saveManager;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->challengeManager = new ChallengeManager();
+        // $this->saveManager = new SaveManager();
+    }
+
     /**
      * Show informations for a specific challenge
      */
-
     public function show(int $id): string
     {
-        $challengeManager = new ChallengeManager();
-        $challenge = $challengeManager->selectOneById($id);
+        $challenge = $this->challengeManager->selectOneById($id);
 
         return $this->twig->render('Challenges/' . $challenge['type'] . '.html.twig', [
             'challenge' => $challenge,
         ]);
     }
 
+    /**
+     * Validate the array, calls saveProgress and if necessary prepareUserSolution
+     */
     public function validate(int $id)
     {
-        $challengeManager = new ChallengeManager();
-        $challenge = $challengeManager->selectOneById($id);
+        $challenge = $this->challengeManager->selectOneById($id);
+
+        // $this->saveManager->saveProgress(7, $id);
+
+        $nextRoomId = $challenge['room_id'];
+        if (($challenge['id'] + 1) % 3 === 1) {
+            $nextRoomId += 1;
+        }
 
         return $this->twig->render('Challenges/validate.html.twig', [
+            'nextRoomId' => $nextRoomId,
             'isCorrect' => $_POST['user_answer'] === $challenge['answer'],
             'challenge' => $challenge
         ]);
